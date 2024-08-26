5f4ac69fc227f49c187d4811023d814b15ebcae9
danieldampe <damper.daniel.sm@gmail.com>
2024-08-25 21:22:31 -0500

    setting deploy

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,14 @@
-import { defineConfig } from 'astro/config';
-import tailwind from '@astrojs/tailwind';
+import { defineConfig } from 'astro/config'
+import tailwind from '@astrojs/tailwind'
 
-import icon from "astro-icon";
+import icon from 'astro-icon'
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), icon()],
   devToolbar: {
     enabled: false
-  }
-});
+  },
+  site: 'https://danieldampe.github.io',
+  base: '/chat-app-css-illustration-master'
+})
